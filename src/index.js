0329272d573aaf209477b540dcf412bf4b0663cc
Victor L <le.victor@hotmail.com>
2022-04-11 08:13:54 -0500

    initial psuedocode

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-function test() {
-    console.log('Working!')
-}
+/**
+To do list
+================
+FUNCTIONS
+.............
+***Base function tile***
+- ADD TASK
+--select multiple box
+- TITLE
+- DESCRIPTION
+- DUE DATE
+--priority
+- EDIT
+- DELETE
+ 
+***Day category***
+- today
+- this week
+- all
 
-test();
+ */
